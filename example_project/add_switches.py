cd30736aabfe63bc46363eb84f42ba691ecd5297
Jeff Pollard <jeff.pollard@gmail.com>
2012-03-05 18:05:02 -0800

    Fixup add_switches script to actually work

--- example_project/add_switches.py
+++ example_project/add_switches.py
@@ -2,9 +2,24 @@ import settings
 
 from gargoyle.client.singleton import gargoyle
 from gargoyle.client.models import Switch, Condition
-from gargoyle.client.operators.identity import Truthy
+from gargoyle.client.operators.misc import Percent
+from gargoyle.client.operators.comparable import Equals, MoreThan
 
-switch = Switch('cool_feature', label='A cool feature')
-condition = Condition(True, Truthy())
+from example_project.arguments import User, Request
+
+switch = Switch('cool_feature', label='A cool feature', description='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+
+condition = Condition(User.name, Equals('Jeff'))
 switch.conditions.append(condition)
+
+condition = Condition(User.age, MoreThan(21))
+switch.conditions.append(condition)
+
+gargoyle.register(switch)
+
+switch = Switch('other_neat_feature', label='A neat additional feature', description='Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+
+condition = Condition(Request.ip, Percent(10))
+switch.conditions.append(condition)
+
 gargoyle.register(switch)
---
+++ example_project/arguments.py
@@ -0,0 +1,13 @@
+class User(object):
+
+    def name(self):
+        return 'Jeff'
+
+    def age(self):
+        return 28
+
+
+class Request(object):
+
+    def ip(self):
+        return '192.168.0.1'
