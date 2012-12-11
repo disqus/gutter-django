1eff49de216d308f2327ac16cfe119bfbef3f182
Jeff Pollard <jeff.pollard@gmail.com>
2012-12-10 17:46:04 -0800

    Refactor the js and css to be more standardized.

--- gutter/web/media/coffee/gutter.coffee
+++ gutter/web/media/coffee/gutter.coffee
@@ -3,31 +3,46 @@ swtch =
   selective: '2'
   global: '3'
 
-ensure_correct_visibility_for = (selects) ->
-  conditions = $(selects).parents('li').find('section.conditions')
+ensure_correct_visibility_for = (row) ->
+  $conditions = $(row).find('section.conditions')
+  $select = $(row).find('select[name=state]')
 
-  switch $(selects).val()
-    when swtch.disabled, swtch.global then conditions.hide()
-    when swtch.selective then conditions.show()
+  switch $select.val()
+    when swtch.disabled, swtch.global then $conditions.hide()
+    when swtch.selective then $conditions.show()
 
-handle_operator_for = (operator) ->
-  $operator = $(operator)
-  $parent = $(operator).parent('li')
+ensure_correct_arguments_for = (row) ->
+  # Remove existing inputs from the row
+  row.find('input[type=text]').remove()
 
-  existing = $parent.find('input[type=text]').remove()
+  $operator = row.find('select[name$="operator"]')
 
   name_prefix = $operator.attr('name').split('-')[0..2].join('-')
   new_arguments = $operator.find('option:selected').data('arguments').split(',')
 
   $.each new_arguments, (index, argument) ->
     input = $('<input>').attr(name: name_prefix + '-' + argument, type: 'text')
-    $operator.parent('li').append(input)
+    row.append(input)
+
+add_condition_to = (row) ->
+  event.preventDefault()
 
 $ ->
-  # Changes to elements
-  $('ul.switches li select[name=state]').change -> ensure_correct_visibility_for(this)
-  $('select[name$="operator"]').change -> handle_operator_for(this)
+
+  $state_selects = $('ul.switches li select[name=state]')
+
+  $state_selects.change (event) ->
+    row = $(this).parents('li')
+    ensure_correct_visibility_for(row)
+
+  $('select[name$="operator"]').change (event) ->
+    row = $(this).parent('li')
+    ensure_correct_arguments_for(row)
+
+  $('ul.switches li section.conditions button[data-action=add]').click (event) ->
+    event.preventDefault()
+    row = $(event.currentTarget).parent('li')[0]
+    add_condition_to(row)
 
   # Setup things for the first time
-  all_state_selects = $('ul.switches li select[name=state]')
-  ensure_correct_visibility_for(all_state_selects)
+  $.map($('ul.switches li'), ensure_correct_visibility_for)
--- gutter/web/media/js/gutter.js
+++ gutter/web/media/js/gutter.js
@@ -1,5 +1,5 @@
 (function() {
-  var ensure_correct_visibility_for, handle_operator_for, swtch;
+  var add_condition_to, ensure_correct_arguments_for, ensure_correct_visibility_for, swtch;
 
   swtch = {
     disabled: '1',
@@ -7,23 +7,23 @@
     global: '3'
   };
 
-  ensure_correct_visibility_for = function(selects) {
-    var conditions;
-    conditions = $(selects).parents('li').find('section.conditions');
-    switch ($(selects).val()) {
+  ensure_correct_visibility_for = function(row) {
+    var $conditions, $select;
+    $conditions = $(row).find('section.conditions');
+    $select = $(row).find('select[name=state]');
+    switch ($select.val()) {
       case swtch.disabled:
       case swtch.global:
-        return conditions.hide();
+        return $conditions.hide();
       case swtch.selective:
-        return conditions.show();
+        return $conditions.show();
     }
   };
 
-  handle_operator_for = function(operator) {
-    var $operator, $parent, existing, name_prefix, new_arguments;
-    $operator = $(operator);
-    $parent = $(operator).parent('li');
-    existing = $parent.find('input[type=text]').remove();
+  ensure_correct_arguments_for = function(row) {
+    var $operator, name_prefix, new_arguments;
+    row.find('input[type=text]').remove();
+    $operator = row.find('select[name$="operator"]');
     name_prefix = $operator.attr('name').split('-').slice(0, 3).join('-');
     new_arguments = $operator.find('option:selected').data('arguments').split(',');
     return $.each(new_arguments, function(index, argument) {
@@ -32,20 +32,34 @@
         name: name_prefix + '-' + argument,
         type: 'text'
       });
-      return $operator.parent('li').append(input);
+      return row.append(input);
     });
   };
 
+  add_condition_to = function(row) {
+    return event.preventDefault();
+  };
+
   $(function() {
-    var all_state_selects;
-    $('ul.switches li select[name=state]').change(function() {
-      return ensure_correct_visibility_for(this);
+    var $state_selects;
+    $state_selects = $('ul.switches li select[name=state]');
+    $state_selects.change(function(event) {
+      var row;
+      row = $(this).parents('li');
+      return ensure_correct_visibility_for(row);
     });
-    $('select[name$="operator"]').change(function() {
-      return handle_operator_for(this);
+    $('select[name$="operator"]').change(function(event) {
+      var row;
+      row = $(this).parent('li');
+      return ensure_correct_arguments_for(row);
     });
-    all_state_selects = $('ul.switches li select[name=state]');
-    return ensure_correct_visibility_for(all_state_selects);
+    $('ul.switches li section.conditions button[data-action=add]').click(function(event) {
+      var row;
+      event.preventDefault();
+      row = $(event.currentTarget).parent('li')[0];
+      return add_condition_to(row);
+    });
+    return $.map($('ul.switches li'), ensure_correct_visibility_for);
   });
 
 }).call(this);
