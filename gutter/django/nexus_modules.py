496a1174811f5dc9aa093faf649cc49e341001a6
Tobias Andersen <tobias.andersen111@gmail.com>
2014-08-14 13:20:38 +0200

    Check for ImportError

--- gutter/django/nexus_modules.py
+++ gutter/django/nexus_modules.py
@@ -46,7 +46,10 @@ class GutterModule(nexus.NexusModule):
         return 'Gutter'
 
     def get_urls(self):
-        from django.conf.urls import patterns, url
+        try:
+            from django.conf.urls import patterns, url
+        except ImportError:
+            from django.conf.urls.defaults import patterns, url
 
         urlpatterns = patterns(
             '',
