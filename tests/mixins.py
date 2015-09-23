efa53b2c2d215e485774b309ea8c6a05e9707a65
Jason Yan <tailofthesun@gmail.com>
2015-09-22 20:00:12 -0700

    Ensure `SetupDjangoTest` cleans up after itself.

--- tests/mixins.py
+++ tests/mixins.py
@@ -1,6 +1,6 @@
 import django
 from django.conf import settings
-from exam import before
+from exam import before, after
 
 
 class SetupDjangoTest(object):
@@ -9,8 +9,16 @@ class SetupDjangoTest(object):
     def setup_django(self):
         # For Django 1.7+, we need to run `django.setup()` first.
         if hasattr(django, 'setup'):
+            self.__original_installed_apps = settings.INSTALLED_APPS
             settings.INSTALLED_APPS = (
                 'django.contrib.auth',
                 'django.contrib.sessions',
             ) + settings.INSTALLED_APPS
+
             django.setup()
+
+    @after
+    def undo_setup_django(self):
+        if hasattr(django, 'setup'):
+            settings.INSTALLED_APPS = self.__original_installed_apps
+            del self.__original_installed_apps
